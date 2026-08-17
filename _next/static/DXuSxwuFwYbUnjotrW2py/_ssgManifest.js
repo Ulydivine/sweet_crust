57ca92c735df8ccef19ff0b15b2c370a4426ffdc
Nkennyelvis <e.nsengiman@alustudent.com>
2026-08-17 16:02:40 +0200

    chore: publish static preview (2026-08-17 14:02)

---
+++ _next/static/DXuSxwuFwYbUnjotrW2py/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fadmin\u002Forders\u002F[id]","\u002Fadmin\u002Fproducts\u002F[id]","\u002Forder\u002F[orderNumber]","\u002Fpatisseries\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
